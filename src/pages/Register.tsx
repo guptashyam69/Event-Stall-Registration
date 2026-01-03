8308b93d588705199044e57867607801638db179
guptashyam69 <guptashyam1121@gmail.com>
2026-01-03 11:44:42 +0530

    Update Register.tsx

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import RegistrationForm from "@/components/RegistrationForm";
 import { Button } from "@/components/ui/button";
